fe55edc0ac9fb6f17e60a4ac101c49d45fa3b265
Victor Morales <electrocucaracha@gmail.com>
2020-12-12 18:54:22 -0800

    Change the trap function

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -21,7 +21,7 @@ function exit_trap {
     awk -v low="$(grep low /proc/zoneinfo | awk '{k+=$2}END{print k}')" '{a[$1]=$2}  END{ print a["MemFree:"]+a["Active(file):"]+a["Inactive(file):"]+a["SReclaimable:"]-(12*low);}' /proc/meminfo
     if command -v kubectl; then
         kubectl get all -A -o wide
-        kubectl get nodes -o wide
+        kubectl describe nodes
     fi
 }
 
