1e4852048b1fe3a7f71e05d20914165cfcbd8076
Victor Morales <chipahuac@hotmail.com>
2021-02-22 15:11:29 -0800

    Add Tekton common tasks

--- ci/tekton/setup.sh
+++ ci/tekton/setup.sh
@@ -12,8 +12,6 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-echo "Configuring Tekton CI pipelines"
-
 # Install dependencies
 pkgs=""
 for pkg in tkn kubectl; do
@@ -22,17 +20,21 @@ for pkg in tkn kubectl; do
     fi
 done
 if [ -n "$pkgs" ]; then
+    echo "Installing Tekton clients"
     # NOTE: Shorten link -> https://github.com/electrocucaracha/pkg-mgr_scripts
     curl -fsSL http://bit.ly/install_pkg | PKG="$pkgs" bash
 fi
 
+echo "Configuring Tekton CI tasks"
 for task in linter vind; do
     kubectl apply -f "https://raw.githubusercontent.com/electrocucaracha/$task/master/tkn.yml"
 done
+kubectl apply -f ./tasks
 
+echo "Configuring Tekton CI pipelines"
 for pipeline in ./pipelines/*.yml; do
     kubectl apply -f "https://raw.githubusercontent.com/electrocucaracha/$(basename "$pipeline" | sed "s/\.yml//")/master/build/ci/tkn.yml"
 done
-
 kubectl apply -f ./pipelines
+
 tkn pipeline list -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'
