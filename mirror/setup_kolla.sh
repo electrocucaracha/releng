0d20a7da9e6a4041698775aef0365d13fa7bc1a0
Victor Morales <electrocucaracha@gmail.com>
2021-05-13 02:48:41 +0000

    Drop kolla profiles usage from build

--- mirror/setup_kolla.sh
+++ mirror/setup_kolla.sh
@@ -9,21 +9,31 @@
 ##############################################################################
 
 set -o pipefail
-set -o xtrace
 set -o errexit
 set -o nounset
-
-if [ "${RELENG_KOLLA_BUID:-false}" == "true" ]; then
-    SNAP=$HOME/.local/ kolla-build --config-file /etc/kolla/kolla-build.ini | jq "." | tee "$HOME/output.json"
-    if [[ $(jq  '.failed | length ' "$HOME/output.json") != 0 ]]; then
-        jq  '.failed[].name' "$HOME/output.json"
-        exit 1
-    fi
-else
-    while IFS= read -r image; do
-        image_name="${image#*/}"
-        if [ "$(curl "http://localhost:5000/v2/${image_name%:*}/tags/list" -o /dev/null -w '%{http_code}\n' -s)" != "200" ]; then
-            skopeo copy --dest-tls-verify=false "docker://$image" "docker://localhost:5000/$image_name"
-        fi
-    done < kolla_images.txt
+if [[ "${DEBUG:-false}" == "true" ]]; then
+    set -o xtrace
 fi
+
+while IFS= read -r image; do
+    image_name="${image#*/}"
+    if [ "$(curl "http://localhost:5000/v2/${image_name%:*}/tags/list" -o /dev/null -w '%{http_code}\n' -s)" != "200" ]; then
+        if [ "${RELENG_KOLLA_BUID:-false}" == "true" ]; then
+            image_tag="${image#*binary-}"
+            SNAP=$HOME/.local/ kolla-build "${image_tag%:*}" --base ubuntu --base-arch "$(uname -m)" \
+            --push --registry localhost:5000 --tag wallaby --squash --skip-existing --noskip-parents | jq "." | tee "$HOME/output.json"
+            if [[ $(jq  '.failed | length ' "$HOME/output.json") != 0 ]]; then
+                jq  '.failed[].name' "$HOME/output.json"
+                exit 1
+            fi
+        else
+            if command -v skopeo; then
+                skopeo copy --dest-tls-verify=false "docker://$image" "docker://localhost:5000/$image_name"
+            else
+                docker pull "$image"
+                docker tag "$image" "localhost:5000/$image_name"
+                docker push "localhost:5000/$image_name"
+            fi
+        fi
+    fi
+done < kolla_images.txt
