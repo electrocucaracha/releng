85010e7abef2333df80b59867591140bf1e82d0b
Victor Morales <chipahuac@hotmail.com>
2021-08-20 15:13:35 -0700

    Fix kolla build instruction

--- mirror/setup_kolla.sh
+++ mirror/setup_kolla.sh
@@ -15,18 +15,24 @@ if [[ "${DEBUG:-false}" == "true" ]]; then
     set -o xtrace
 fi
 
-while IFS= read -r image; do
-    image_name="${image#*/}"
-    if [ "$(curl "http://localhost:5000/v2/${image_name%:*}/tags/list" -o /dev/null -w '%{http_code}\n' -s)" != "200" ] || [ "$(curl "http://localhost:5000/v2/${image_name%:*}/manifests/${image_name#*:}" -o /dev/null -w '%{http_code}\n' -s)" != "200" ]; then
-        if [ "${RELENG_KOLLA_BUILD:-false}" == "true" ]; then
-            image_tag="${image#*binary-}"
-            SNAP=$HOME/.local/ kolla-build "${image_tag%:*}" --base ubuntu --base-arch "$(uname -m)" \
-            --push --registry localhost:5000 --tag wallaby --squash --skip-existing --noskip-parents | jq "." | tee "$HOME/output.json"
-            if [[ $(jq  '.failed | length ' "$HOME/output.json") != 0 ]]; then
-                jq  '.failed[].name' "$HOME/output.json"
-                exit 1
-            fi
-        else
+if [ "${RELENG_KOLLA_BUILD:-false}" == "true" ]; then
+    image_tag="${image#*binary-}"
+    newgrp docker <<EONG
+    # PEP 370 -- Per user site-packages directory
+    [[ "$PATH" != *.local/bin* ]] && export PATH=$PATH:$HOME/.local/bin
+    SNAP=$HOME/.local/ kolla-build "${image_tag%:*}" --base ubuntu \
+    --base-arch "$(uname -m)" --push --registry localhost:5000 \
+    --tag wallaby --squash --skip-existing --noskip-parents \
+    | jq "." | tee "$HOME/output.json"
+EONG
+    if [[ $(jq  '.failed | length ' "$HOME/output.json") != 0 ]]; then
+        jq  '.failed[].name' "$HOME/output.json"
+        exit 1
+    fi
+else
+    while IFS= read -r image; do
+        image_name="${image#*/}"
+        if [ "$(curl "http://localhost:5000/v2/${image_name%:*}/tags/list" -o /dev/null -w '%{http_code}\n' -s)" != "200" ] || [ "$(curl "http://localhost:5000/v2/${image_name%:*}/manifests/${image_name#*:}" -o /dev/null -w '%{http_code}\n' -s)" != "200" ]; then
             if command -v skopeo; then
                 skopeo copy --dest-tls-verify=false "docker://$image" "docker://localhost:5000/$image_name"
             else
@@ -35,5 +41,5 @@ while IFS= read -r image; do
                 docker push "localhost:5000/$image_name"
             fi
         fi
-    fi
-done < kolla_images.txt
+    done < kolla_images.txt
+fi
